38bccd20fedad8340ec2ce3b0179f5c41bc30f2b
facucompe <facu.compe@gmail.com>
2017-09-01 14:41:01 -0300

    Add session controller, routes authentication modules

---
+++ app/authentication/authentication_unique_token.rb
@@ -0,0 +1,10 @@
+
+# app/authentication/authentication_unique_token.rb
+class AuthenticationUniqueToken
+  class << self
+    # I'm not checking the uniqueness because its unlikely to happen
+    def generate
+      SecureRandom.hex(16)
+    end
+  end
+end
