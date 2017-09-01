7ea4616649624097d0676cb1535345bc501e7d81
facucompe <facu.compe@gmail.com>
2017-09-01 15:22:50 -0300

    Create a migration and a model for books

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831200741) do
+ActiveRecord::Schema.define(version: 20170901180428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "genre", default: "", null: false
+    t.string "author", default: "", null: false
+    t.string "image", default: "", null: false
+    t.string "title", default: "", null: false
+    t.string "publisher", default: "", null: false
+    t.string "year", default: "", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
