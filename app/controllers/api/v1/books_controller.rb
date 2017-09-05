df42a085af81d082673ee3bef72b30a9710d6434
facucompe <facu.compe@gmail.com>
2017-09-05 15:06:14 -0300

    Style correction

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class BooksController < ApiController
-      
       def index
         render json: Book.all, status: :ok
       end
@@ -9,7 +8,6 @@ module Api
       def show
         render json: Book.find(params[:id]), status: :ok
       end
-
     end
   end
 end
