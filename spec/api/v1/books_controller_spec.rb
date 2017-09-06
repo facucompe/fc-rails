839f29a3ee66affbdfe67172a397cc0baa55cd33
facucompe <facu.compe@gmail.com>
2017-09-06 15:13:57 -0300

    Style corrections

--- spec/api/v1/books_controller_spec.rb
+++ spec/api/v1/books_controller_spec.rb
@@ -15,7 +15,6 @@ describe Api::V1::BooksController, type: :controller do
         expected = ActiveModel::Serializer::CollectionSerializer.new(
           books, each_serializer: BookSerializer
         ).to_json
-        byebug
         expect(response.body.to_json) =~ JSON.parse(expected)
       end
 
@@ -30,11 +29,11 @@ describe Api::V1::BooksController, type: :controller do
       let!(:book) { create(:book) }
 
       before do
-        get :show, params: {  id: book.id }
+        get :show, params: { id: book.id }
       end
 
       it 'responses with the user rent json' do
-        expect(response.body).to eq BookSerializer.new( book ).to_json
+        expect(response.body).to eq BookSerializer.new(book).to_json
       end
 
       it 'responds with 200 status' do
