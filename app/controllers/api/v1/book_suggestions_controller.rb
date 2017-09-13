441aa770fdd558d33a520181644bc81cf8d3c146
facucompe <facu.compe@gmail.com>
2017-09-13 15:56:14 -0300

    Corrections in bookSuggestions controller

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -2,14 +2,15 @@ module Api
   module V1
     class BookSuggestionsController < ApiController
       def create
-        book_suggestion = BookSuggestion.new(suggestion_params)
-        book_suggestion.user_id = current_user.id
+        book_suggestion = BookSuggestion.new(suggestion_params.merge(user_id: current_user.id))
         if book_suggestion.save
           render json: book_suggestion, status: :created
         else
           render json: { errors: book_suggestion.errors }, status: :bad_request
         end
       end
+
+      private
 
       def suggestion_params
         params.require(:book_suggestion).permit(:editorial, :title, :author, :link, :price)
