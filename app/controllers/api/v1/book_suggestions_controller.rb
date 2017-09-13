c3dc1b3da146454a651199fc49a96698d3b6680f
facucompe <facu.compe@gmail.com>
2017-09-13 12:22:28 -0300

    Save book suggestion in db

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -4,9 +4,12 @@ module Api
       skip_before_action :current_user, :authenticate_request
       def create
         book_suggestion = BookSuggestion.new(suggestion_params)
-        byebug
-        render json: { hola: 'hola'}, status: :ok
-      end
+        if book_suggestion.save
+          render json: book_suggestion, status: :created
+        else
+          render json { errors: book_suggestion.errors }, status: :bad_request
+        end
+        end
       def suggestion_params
         params.require(:book_suggestion).permit(:editorial, :title, :author, :link, :price)
       end
