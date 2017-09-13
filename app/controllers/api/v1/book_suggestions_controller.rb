2fe07cb0e45fe63ffa76c0efe2e3e86e107bb438
facucompe <facu.compe@gmail.com>
2017-09-13 13:03:27 -0300

    Add user_id of the current user to the bookSuggestion

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -1,13 +1,13 @@
 module Api
   module V1
     class BookSuggestionsController < ApiController
-      skip_before_action :current_user, :authenticate_request
       def create
         book_suggestion = BookSuggestion.new(suggestion_params)
+        book_suggestion.user_id = current_user.id
         if book_suggestion.save
           render json: book_suggestion, status: :created
         else
-          render json { errors: book_suggestion.errors }, status: :bad_request
+          render json: { errors: book_suggestion.errors }, status: :bad_request
         end
         end
       def suggestion_params
