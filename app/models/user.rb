beee6fa40448756e3522185b758caef29d07c34d
facucompe <facu.compe@gmail.com>
2017-09-18 14:08:11 -0300

    Style correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,15 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(email: data['email']).first
+    user = User.find_by(email: data['email'])
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
-        user = User.create(name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
+      user = User.create(
+        name: data['name'],
+        email: data['email'],
+        password: Devise.friendly_token[0, 20]
+      )
     end
     user
   end
